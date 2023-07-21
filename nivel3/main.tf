bf73a452529a6fd0be8ae51c0ff1decb0b0f71af
Jorge <jorge@mbapizcaazucar.local>
2023-07-21 16:47:51 -0500

    nivel 2 y 3

--- nivel3/main.tf
+++ nivel3/main.tf
@@ -1,4 +1,9 @@
 terraform {
+backend "s3" {
+    bucket="jv-093670503449"
+    key="meli/test/cache"
+    region="us-east-1"
+}
   required_providers {
     aws = {
         source = "hashicorp/aws"
